a6458ecacd9928952ffdc5179a9d7b27bdfaf88e
Thierry de Pauw <thierry@thinkinglabs.io>
2016-10-13 01:03:26 +0200

    move isEmpty() to a Strings class

--- src/test/java/be/tdpauw/tdd_katas/stringcalculator/StringCalculatorTest.java
+++ src/test/java/be/tdpauw/tdd_katas/stringcalculator/StringCalculatorTest.java
@@ -1,8 +1,11 @@
 package be.tdpauw.tdd_katas.stringcalculator;
 
+import static be.tdpauw.tdd_katas.utils.Strings.isEmpty;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+
+import be.tdpauw.tdd_katas.utils.Strings;
 
 public class StringCalculatorTest {
 
@@ -26,14 +29,11 @@ public class StringCalculatorTest {
 		assertThat(add("-1")).isEqualTo(-1);
 	}
 
-	private int add(String numbers) {
+	public static int add(String numbers) {
 		if (isEmpty(numbers)) {
 			return 0;
 		}
 		return Integer.valueOf(numbers);
 	}
 
-	private boolean isEmpty(String numbers) {
-		return "".equals(numbers);
-	}
 }
---
+++ src/test/java/be/tdpauw/tdd_katas/utils/Strings.java
@@ -0,0 +1,12 @@
+package be.tdpauw.tdd_katas.utils;
+
+public class Strings {
+
+	private Strings() {
+		//do nothing
+	}
+
+	public static boolean isEmpty(String numbers) {
+		return "".equals(numbers);
+	}
+}
