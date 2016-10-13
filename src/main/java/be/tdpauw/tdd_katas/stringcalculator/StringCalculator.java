2dbf49df7b21d86896d4bb60e66337a16639feb1
Thierry de Pauw <thierry@thinkinglabs.io>
2016-10-13 08:17:06 +0200

    remove obsolete code now split returns an empty string array for an empty string

--- src/main/java/be/tdpauw/tdd_katas/stringcalculator/StringCalculator.java
+++ src/main/java/be/tdpauw/tdd_katas/stringcalculator/StringCalculator.java
@@ -1,5 +1,7 @@
 package be.tdpauw.tdd_katas.stringcalculator;
 
+import static be.tdpauw.tdd_katas.utils.Strings.split;
+
 import java.util.Arrays;
 
 import be.tdpauw.tdd_katas.utils.Strings;
@@ -7,11 +9,7 @@ import be.tdpauw.tdd_katas.utils.Strings;
 public class StringCalculator {
 
 	public int add(String input) {
-		if (Strings.isEmpty(input)) {
-			return 0;
-		}
-
-		String[] numbers = Strings.split(input);
+		String[] numbers = split(input);
 
 		int result = Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
 		return result;
