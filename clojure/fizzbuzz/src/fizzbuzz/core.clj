1fe365f7b5cef06b76884419d199be458822935b
Thierry de Pauw <thierry@thinkinglabs.io>
2018-09-06 13:25:21 +0200

    extract method for divisible by three

--- clojure/fizzbuzz/src/fizzbuzz/core.clj
+++ clojure/fizzbuzz/src/fizzbuzz/core.clj
@@ -1,9 +1,13 @@
 (ns fizzbuzz.core)
 
+(defn divisible-by-three [input]
+  (zero? (mod input 3))
+)
+
 (defn fizz-buzz [input]
   (cond
      (= input 3) "fizz"
      (= input 5) "buzz"
-     (zero? (mod input 3)) "fizz"
+     (divisible-by-three input) "fizz"
      :else input)
 )
