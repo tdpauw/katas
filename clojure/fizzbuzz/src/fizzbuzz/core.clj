f186bbfb1f583f54643082916c0406d1952c7140
Thierry de Pauw <thierry@thinkinglabs.io>
2018-09-06 13:30:42 +0200

    extract method divisible by five

--- clojure/fizzbuzz/src/fizzbuzz/core.clj
+++ clojure/fizzbuzz/src/fizzbuzz/core.clj
@@ -4,10 +4,14 @@
   (zero? (mod input 3))
 )
 
+(defn divisible-by-five [input]
+  (zero? (mod input 5))
+)
+
 (defn fizz-buzz [input]
   (cond
      (= input 5) "buzz"
      (divisible-by-three input) "fizz"
-     (zero? (mod input 5)) "buzz"
+     (divisible-by-five input) "buzz"
      :else input)
 )
